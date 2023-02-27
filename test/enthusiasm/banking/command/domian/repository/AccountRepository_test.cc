2867092d0456f160a823e79808cf5b4595f4bb64
FineRoot1253 <dev.fineroot1253@gmail.com>
2023-02-27 22:21:20 +0900

    test(/test/enthusiasm/banking/command/domain): FS-84 기존 뱅킹 예제 테스트 코드 수정
    - 요구사항:
     - 공통 요구 사항
      - 계좌개설시 이자율 추가
      - 각 계좌에 입금시 입금액에 입금액 * 이자율 만큼 더해서 입금할 것
     - 계좌 종류 입력 추가
      - 보통예금계좌
       - 1 입력시 보통예금계좌로 개설될 것
      - 신용신뢰계좌
       - 2 입력시 신용신뢰계좌로 개설될 것
       - 신용계좌개설시 추가로 신용등급을 unsigned int로 입력 받을 것
        - 1 입력시 A
        - 2 입력시 B
        - 3 입력시 C
       - 신용계좌 입금시 입금액에 입금액 * 신용등급에 따른 이자율을 더해서 입금할 것
        - 신용등급이 A일시 7%
        - 신용등급이 B일시 4%
        - 신용등급이 C일시 2%
    
    modify entity/Account_test.cc
    modify repository/AccountRepository_test.cc
    계좌 클래스 수정에 따른 테스트 코드 수정
    
    modify repository/CMakeLists.txt
    보통예금계좌, 신용신뢰계좌 클래스 추가에 따른 라이브러리 링킹 추가

--- test/enthusiasm/banking/command/domian/repository/AccountRepository_test.cc
+++ test/enthusiasm/banking/command/domian/repository/AccountRepository_test.cc
@@ -1,85 +1,105 @@
 #include <gtest/gtest.h>
 #include "enthusiasm/banking/command/domain/repository/AccountRepository.h"
+#include "enthusiasm/banking/command/domain/entity/HighCreditAccount.h"
 
 class AccountRepositoryFixture : public ::testing::Test {
 protected:
-    virtual void TearDown() {
-    }
-
     virtual void SetUp() {
-        Account ** temp = new Account * [4];
-        for (int i = 0; i < 4; ++i) {
-            temp[i] = new Account(dummyAccounts[i]);
+        accountRepository = new AccountRepository();
+        for (int i = 0; i < DUMMY_ACCOUNT_COUNT; ++i) {
+            accountRepository->saveAccount(dummyInputDtos[i]);
         }
-       const Account* tempDto [4]= {temp[0],temp[1],temp[2],temp[3]};
-        accountRepository = new AccountRepository(tempDto, 4);
-        for (int i = 0; i < 4; ++i) {
-            delete temp[i];
-        }
-        delete []temp;
     }
 
 public:
-    AccountRepositoryFixture() : Test() {
+    AccountRepositoryFixture()
+            : Test() {
     }
 
     virtual  ~AccountRepositoryFixture() {
         delete accountRepository;
+        for (int i = 0; i < DUMMY_ACCOUNT_COUNT; ++i) {
+            delete dummyAccounts[i];
+        }
     }
 
     AccountRepository *accountRepository;
-    const Account dummyAccounts[4] = {Account{1l,100000, "Hong"},Account{2l,200000, "Lee"},Account{3l,300000, "Kim"},Account{4l,400000, "Choi"}};
-    const int DUMMY_ACCOUNT_COUNT = 4;
+    const int DUMMY_ACCOUNT_COUNT = 8;
+    const AccountInputDto dummyInputDtos[8] = {
+            AccountInputDto(1L, 10000, "Hong", 0.01),
+            AccountInputDto(2L, 20000, "Kim", 0.02),
+            AccountInputDto(3L, 30000, "Lee", 0.03),
+            AccountInputDto(4L, 40000, "Park", 0.04),
+            AccountInputDto(5L, 50000, "Hong", 0.05,
+                            CreditGradeInputDto(utils::Parser::creditGradeToInt(CreditGrade::A))),
+            AccountInputDto(6L, 50000, "Gong", 0.05,
+                            CreditGradeInputDto(utils::Parser::creditGradeToInt(CreditGrade::B))),
+            AccountInputDto(7L, 50000, "Kim", 0.05,
+                            CreditGradeInputDto(utils::Parser::creditGradeToInt(CreditGrade::C))),
+            AccountInputDto()
+    };
+    const Account *dummyAccounts[8] = {
+            new NormalAccount(1L, 10000, "Hong", 0.01),
+            new NormalAccount(2L, 20000, "Kim", 0.02),
+            new NormalAccount(3L, 30000, "Lee", 0.03),
+            new NormalAccount(4L, 40000, "Park", 0.04),
+            new HighCreditAccount(5L, 50000, "Hong", 0.05, CreditGrade::A),
+            new HighCreditAccount(6L, 50000, "Gong", 0.05, CreditGrade::B),
+            new HighCreditAccount(7L, 50000, "Kim", 0.05, CreditGrade::C),
+            new NormalAccount()
+    };
 };
 
 TEST_F(AccountRepositoryFixture, setRepository) {
-    AccountRepository dummy = *accountRepository;
-    for (int i = 0; i < DUMMY_ACCOUNT_COUNT; ++i) {
-        const BaseReturnDto<Account> &dto = dummy.findById(dummyAccounts->getAccId());
-        EXPECT_EQ(dummyAccounts->getAccId(), dto.data.getAccId());
-        EXPECT_STREQ(dummyAccounts->getCusName(),dto.data.getCusName());
-        EXPECT_EQ(dummyAccounts->getBalance(), dto.data.getBalance());
+    try {
+        AccountRepository dummy = *accountRepository;
+        for (int i = 0; i < DUMMY_ACCOUNT_COUNT; ++i) {
+            const BaseReturnDto<Account*> &dto = dummy.findById(dummyAccounts[i]->getAccId());
+            EXPECT_EQ(dummyAccounts[i]->getAccId(), dto.data->getAccId());
+            EXPECT_STREQ(dummyAccounts[i]->getCusName(), dto.data->getCusName());
+            EXPECT_EQ(dummyAccounts[i]->getBalance(), dto.data->getBalance());
+        }
+    } catch (IException &exception) {
+        std::cout << "exception: " << exception.getReason() << std::endl;
     }
+
 }
 
 TEST_F(AccountRepositoryFixture, saveAccount) {
     AccountRepository dummy = *accountRepository;
-    const HGrowableList<Account>& accounts = dummy.findAll();
-    AccountRepository *compareRepository = new AccountRepository({},0);
+    const HGrowableList<Account *> &accounts = dummy.findAll();
+    AccountRepository *compareRepository = new AccountRepository();
     for (int i = 0; i < dummy.getTotalCounts().data; ++i) {
-        const Account &account = accounts.get(i);
-        compareRepository->saveAccount(AccountInputDto{account.getAccId(),account.getBalance(),account.getCusName()});
+        const Account *account = accounts.get(i);
+        compareRepository->saveAccount(
+                AccountInputDto{account->getAccId(), account->getBalance(), account->getCusName()});
     }
-    compareRepository->saveAccount(AccountInputDto{5l, 500000, "Park"});
-    EXPECT_EQ(compareRepository->getTotalCounts().data, 5);
-    if(!compareRepository->findById(5l).error.isError()){
-        const Account &account = compareRepository->findById(5l).data;
-        EXPECT_STREQ(account.getCusName(), "Park");
-        EXPECT_EQ(account.getBalance(), 500000);
-    }
+    const Error &result = compareRepository->saveAccount(AccountInputDto{5l, 500000, "Park"});
+    EXPECT_EQ(result.isError(), true);
+    EXPECT_STREQ(result.getReason(), constants::err_kr::ERR_MSG_ACCOUNT_ID_DUPLICATED);
     delete compareRepository;
 }
 
 TEST_F(AccountRepositoryFixture, depositAccount) {
     AccountRepository dummy = *accountRepository;
-    const Error &error1 = dummy.saveAccount(AccountInputDto{5l, 100000, "Park"});
-    const Error &error2 = dummy.saveBalance(BalanceDto{5l, 100000, eBankingMode::Deposit});
-    const Account &data =dummy.findById(5l).data;
-    EXPECT_EQ(data.getBalance(), 200000);
+    const Error &error1 = dummy.saveAccount(AccountInputDto{9L, 100000, "Park"});
+    const Error &error2 = dummy.saveBalance(BalanceDto{9L, 100000, eBankingMode::Deposit});
+    const Account *data = dummy.findById(9L).data;
+    EXPECT_EQ(data->getBalance(), 200000);
 }
 
 TEST_F(AccountRepositoryFixture, withdrawAccount) {
     AccountRepository dummy = *accountRepository;
-    const Error &error1 = dummy.saveAccount(AccountInputDto{5l, 100000, "Park"});
-    const Error &error2 = dummy.saveBalance(BalanceDto{5l, 100000, eBankingMode::WithDraw});
-    const Account &data =dummy.findById(5l).data;
-    EXPECT_EQ(data.getBalance(), 0);
-    const Error &error3 = dummy.saveBalance(BalanceDto{5l, 100000, eBankingMode::WithDraw});
+    const Error &error1 = dummy.saveAccount(AccountInputDto{9L, 100000, "Park"});
+    const Error &error2 = dummy.saveBalance(BalanceDto{9L, 100000, eBankingMode::WithDraw});
+    const Account *data = dummy.findById(9L).data;
+    EXPECT_EQ(data->getBalance(), 0);
+    const Error &error3 = dummy.saveBalance(BalanceDto{9L, 100000, eBankingMode::WithDraw});
     EXPECT_EQ(error3.isError(), true);
-    EXPECT_STREQ(error3.getReason(),"너무 많은 액수를 출금");
+    EXPECT_STREQ(error3.getReason(), "너무 많은 액수를 출금");
 }
 
 TEST_F(AccountRepositoryFixture, getUserCounts) {
     AccountRepository dummy = *accountRepository;
-    EXPECT_EQ(dummy.getTotalCounts().data, 4);
+    EXPECT_EQ(dummy.getTotalCounts().data, DUMMY_ACCOUNT_COUNT);
 }
