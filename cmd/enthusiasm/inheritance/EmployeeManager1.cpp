12f5061e4dd849ee607b6482b22dc35f5616171e
FineRoot1253 <dev.fineroot1253@gmail.com>
2023-03-03 15:40:41 +0900

    feat(/cmd/enthusiasm/inheritance): FS-83 누락파일 추가
    add EmployeeManager1.cpp
    main entry 실행 소스파일 추가

---
+++ cmd/enthusiasm/inheritance/EmployeeManager1.cpp
@@ -0,0 +1,16 @@
+#include "enthusiasm/inheritance/EmployeeManager1.h"
+
+int main(){
+    EmployeeHandler handler;
+
+    handler.addEmployee(new PermanentWorker("KIM", 1000));
+    handler.addEmployee(new PermanentWorker("LEE", 1500));
+    handler.addEmployee(new PermanentWorker("JUN", 2000));
+
+    handler.showAllSalaryInfo();
+
+    handler.showTotalSalaryInfo();
+    return 0;
+}
+
+
